de94f6ebefd26531717a1602cb11e9f9f7009353
Feng Zhu <lyricorpse@gmail.com>
2019-09-24 15:20:17 -0700

    Add butter_lowpass_filter()

--- LMRt/utils.py
+++ LMRt/utils.py
@@ -4870,3 +4870,18 @@ def phaseran(recblk, nsurr):
         surrblk[:, k] = np.real(np.fft.ifft(fft_recblk_surr))
 
     return surrblk
+
+# -----------------------------------------------
+#  filters
+# -----------------------------------------------
+def butter_lowpass(cutoff, fs, order=5):
+    nyq = 0.5 * fs
+    normal_cutoff = cutoff / nyq
+    b, a = signal.butter(order, normal_cutoff, btype='low', analog=False)
+    return b, a
+
+
+def butter_lowpass_filter(data, cutoff, fs, order=5):
+    b, a = butter_lowpass(cutoff, fs, order=order)
+    y = signal.filtfilt(b, a, data)
+    return y
