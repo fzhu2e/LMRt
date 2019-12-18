a4f5a8a8cfcd0376e952a548c7fb8dce3980db37
Feng Zhu <lyricorpse@gmail.com>
2019-12-17 18:40:53 -0800

    Fix the order of month in seasonality examples

--- LMRt/visual.py
+++ LMRt/visual.py
@@ -1065,11 +1065,13 @@ def plot_volc_composites(gmt, event_yrs, start_yr=0, before=3, after=10, highpas
 
 def plot_sea_res(res, style='ticks', font_scale=2, figsize=[6, 6], signif_fontsize=15, ls='-o', lw=3, color='k', label=None,
                  shade_alpha=0.3, signif_alpha=0.3, signif_color='k', signif_text_loc_fix=(0.1, -0.01),
-                 xticks=None, title=None, plot_signif=True):
+                 xlabel='Years relative to event year', ylabel='T anom. (K)',
+                 xticks=None, title=None, plot_signif=True, fig=None, ax=None):
     ''' Plot SEA results
     '''
-    sns.set(style=style, font_scale=font_scale)
-    fig, ax = plt.subplots(figsize=figsize)
+    if ax is None or fig is None:
+        sns.set(style=style, font_scale=font_scale)
+        fig, ax = plt.subplots(figsize=figsize)
 
     if 'composite_qs' in res.keys():
         ax.plot(res['composite_yr'], res['composite_qs'][1], ls, color=color, label=label, lw=lw)
@@ -1086,8 +1088,8 @@ def plot_sea_res(res, style='ticks', font_scale=2, figsize=[6, 6], signif_fontsi
             ax.text(res['composite_yr'][-1]+signif_text_loc_fix[0], res['composite_qs_signif'][i][-1]+signif_text_loc_fix[-1],
                     f'{qs_v*100:g}%', color=signif_color, alpha=signif_alpha, fontsize=signif_fontsize)
 
-    ax.set_ylabel('T anom. (K)')
-    ax.set_xlabel('Years relative to event year')
+    ax.set_ylabel(ylabel)
+    ax.set_xlabel(xlabel)
     ax.axvline(x=0, ls=':', color='grey')
     ax.axhline(y=0, ls=':', color='grey')
     ax.spines['right'].set_visible(False)
